fcf4b9ee187661e896e779d4bc0c1c5527a1293c
Nuttachai-K <korn.jp.nuttachai@gmail.com>
2026-07-18 18:06:41 +0900

    Add cafe update model for update query

--- internal/model/cafe.go
+++ internal/model/cafe.go
@@ -8,10 +8,20 @@ type Cafe struct {
 	Name           string    `json:"name"`
 	Address        string    `json:"address"`
 	Latitude       float64   `json:"latitude"`
-	Longitude      float64   `json:"longtitude"`
+	Longitude      float64   `json:"longitude"`
 	NearestStation string    `json:"nearest_station"`
 	OpeningTime    time.Time `json:"opening_time"`
 	ClosingTime    time.Time `json:"closing_time"`
 	CreatedAt      time.Time `json:"created_at"`
 	UpdatedAt      time.Time `json:"updated_at"`
+}
+
+type CafeUpdate struct {
+	Name           string    `json:"name,omitempty"`
+	Address        string    `json:"address,omitempty"`
+	Latitude       float64   `json:"latitude,omitempty"`
+	Longitude      float64   `json:"longitude,omitempty"`
+	NearestStation string    `json:"nearest_station,omitempty"`
+	OpeningTime    time.Time `json:"opening_time,omitempty"`
+	ClosingTime    time.Time `json:"closing_time,omitempty"`
 }
