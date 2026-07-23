f4d9e69c17f83f2f7a547d961bbb624d461739e6
Nuttachai-K <korn.jp.nuttachai@gmail.com>
2026-07-23 18:03:58 +0900

    Fix migrate crete user table role to role user

--- migrations/000003_create_users_table.up.sql
+++ migrations/000003_create_users_table.up.sql
@@ -3,7 +3,7 @@ CREATE TABLE users (
     username VARCHAR(100) NOT NULL,
     email VARCHAR(255) NOT NULL UNIQUE,
     password_hash TEXT NOT NULL,
-    user_role VARCHAR(10) NOT NULL CHECK (role IN ('ADMIN', 'STAFF')),
+    user_role VARCHAR(10) NOT NULL CHECK (user_role IN ('ADMIN', 'STAFF')),
     created_at TIMESTAMP NOT NULL DEFAULT NOW(),
     updated_at TIMESTAMP NOT NULL DEFAULT NOW()
 );
