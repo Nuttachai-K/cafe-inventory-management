946452ab90fde018876279d96dcca98d11daa6a2
Nuttachai-K <korn.jp.nuttachai@gmail.com>
2026-07-25 14:59:40 +0900

    Correct api name

--- internal/router/router.go
+++ internal/router/router.go
@@ -10,17 +10,17 @@ func NewRouter(cafeHandler *handler.CafeHandler, userHandler *handler.UserHandle
 
 	mux := http.NewServeMux()
 
-	mux.HandleFunc("GET /api/v1//cafes", cafeHandler.GetAll)
-	mux.HandleFunc("GET /api/v1//cafes/{id}", cafeHandler.GetByID)
-	mux.HandleFunc("POST /api/v1//cafes", cafeHandler.Create)
-	mux.HandleFunc("PATCH /api/v1//cafes/{id}", cafeHandler.Update)
-	mux.HandleFunc("DELETE /api/v1//cafes/{id}", cafeHandler.Delete)
+	mux.HandleFunc("GET /api/v1/cafes", cafeHandler.GetAll)
+	mux.HandleFunc("GET /api/v1/cafes/{id}", cafeHandler.GetByID)
+	mux.HandleFunc("POST /api/v1/cafes", cafeHandler.Create)
+	mux.HandleFunc("PATCH /api/v1/cafes/{id}", cafeHandler.Update)
+	mux.HandleFunc("DELETE /api/v1/cafes/{id}", cafeHandler.Delete)
 
-	mux.HandleFunc("GET /api/v1//users", userHandler.GetAll)
-	mux.HandleFunc("GET /api/v1//users/{id}", userHandler.GetByID)
-	mux.HandleFunc("POST /api/v1//users", userHandler.Create)
-	mux.HandleFunc("PATCH /api/v1//users/{id}", userHandler.Update)
-	mux.HandleFunc("DELETE /api/v1//users/{id}", userHandler.Delete)
+	mux.HandleFunc("GET /api/v1/users", userHandler.GetAll)
+	mux.HandleFunc("GET /api/v1/users/{id}", userHandler.GetByID)
+	mux.HandleFunc("POST /api/v1/users", userHandler.Create)
+	mux.HandleFunc("PATCH /api/v1/users/{id}", userHandler.Update)
+	mux.HandleFunc("DELETE /api/v1/users/{id}", userHandler.Delete)
 
 	return mux
 }
