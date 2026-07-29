1ec1b2ff87344147a22603537c8da240672b4701
Nuttachai-K <korn.jp.nuttachai@gmail.com>
2026-07-29 11:08:50 +0900

    Add create categories and products table migration codes

---
+++ migrations/000005_create_products_table.up.sql
@@ -0,0 +1,15 @@
+CREATE TABLE products (
+    id SERIAL PRIMARY KEY,
+    cafe_id INT REFERENCES cafes(id) NOT NULL,
+    category_id INT REFERENCES categories(id) NOT NULL,
+    name TEXT NOT NULL,
+    description TEXT,
+    price NUMERIC(10,2) NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMP NOT NULL DEFAULT NOW()
+);
+
+CREATE TRIGGER update_products_updated_at
+BEFORE UPDATE ON products
+FOR EACH ROW
+EXECUTE FUNCTION update_updated_at_column();
