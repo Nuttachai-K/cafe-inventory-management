809c6dac5725a020bbcb39d2e40067265b1027f7
Nuttachai-K <korn.jp.nuttachai@gmail.com>
2026-07-29 14:10:29 +0900

    Implement product model and repository

---
+++ internal/model/product.go
@@ -0,0 +1,25 @@
+package model
+
+import (
+	"time"
+
+	"github.com/shopspring/decimal"
+)
+
+type Product struct {
+	ID          int             `json:"id"`
+	CafeId      int             `json:"cafe_id"`
+	CategoryId  int             `json:"category_id"`
+	Name        string          `json:"name"`
+	Description string          `json:"description"`
+	Price       decimal.Decimal `json:"price"`
+	CreatedAt   time.Time       `json:"created_at"`
+	UpdatedAt   time.Time       `json:"updated_at"`
+}
+type ProductUpdate struct {
+	CafeId      *int             `json:"cafe_id"`
+	CategoryId  *int             `json:"category_id"`
+	Name        *string          `json:"name"`
+	Description *string          `json:"description"`
+	Price       *decimal.Decimal `json:"price"`
+}
